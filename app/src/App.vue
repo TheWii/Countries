1a176ae53250fd14c7fd8f0a40c16b1e85df0f2b
TheWii <67249660+TheWii@users.noreply.github.com>
2021-11-06 19:02:08 -0300

    Add Vue app and other important files
    Add initial vue files generated with Vue CLI as well as README.

---
+++ app/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+<div>
+  <h1>Where in the world?</h1>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ app/vue.config.js
@@ -0,0 +1,7 @@
+
+const path = require('path');
+
+module.exports = {
+    outputDir: path.resolve('../public'),
+    publicPath: process.env.NODE_ENV === 'production' ? './' : '/'
+}
